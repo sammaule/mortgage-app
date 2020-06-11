3f1edf642d62cb71ef43d2aac85bd8c9a4ac0238
smaule <smaule@deloitte.co.uk>
2020-06-11 18:35:08 +0100

    wip

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import numpy as np
+from dash.exceptions import PreventUpdate
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -22,31 +23,30 @@ app.layout = html.Div([
     ),
     # TODO: Align the input boxes
     html.Div([
-        "Mortgage size (£k)",
-        dcc.Input(id='mortgage-size', value=200, type='number'),
+        "Deposit size (£k)",
+        dcc.Input(id='deposit-size', value=150, type='number'),
     ]),
     html.Div([
         "Purchase price (£k)",
-        dcc.Input(id='purchase-price', value=350, type='number'),
+        dcc.Input(id='purchase-price', value=375, type='number'),
     ]),
     html.Div(id="ltv"
              ),
     html.Div([
         "Offer term (years): ",
-        # TODO: Write callback so max offer term can't be longer than current mortgage term
         dcc.Input(id='offer-term', value=2, type='number', min=0),
     ]),
     html.Div([
         "Mortgage term (years): ",
-        dcc.Input(id='mortgage-term', value=20, type='number', min=0),
+        dcc.Input(id='mortgage-term', value=20, type='number', min=1),
     ]),
     html.Div([
         "Initial interest rate (%): ",
-        dcc.Input(id='initial-interest-rate', value=1.0, type='number', min=0, max=100),
+        dcc.Input(id='initial-interest-rate', value=1.05, type='number', min=0.01, max=100),
     ]),
     html.Div([
         "Interest rate (%): ",
-        dcc.Input(id='interest-rate', value=2.0, type='number', min=0, max=100),
+        dcc.Input(id='interest-rate', value=3.0, type='number', min=0.01, max=100),
     ]),
     html.Div(id="total-repaid"
              ),
@@ -55,11 +55,11 @@ app.layout = html.Div([
 
 @app.callback(
     Output('ltv', 'children'),
-    [Input('mortgage-size', 'value'),
+    [Input('deposit-size', 'value'),
      Input('purchase-price', 'value')]
 )
-def calc_ltv(mortgage, price):
-    ltv = round(mortgage * 100 / price, 1)
+def calc_ltv(deposit, price):
+    ltv = round((price - deposit) * 100 / price, 1)
     return f"LTV: {ltv}%"
 
 
@@ -67,19 +67,21 @@ def calc_ltv(mortgage, price):
     [Output('mortgage-plot', 'figure'),
      Output('total-repaid', 'children')
      ],
-    [Input('mortgage-size', 'value'),
+    [Input('deposit-size', 'value'),
+     Input('purchase-price', 'value'),
      Input('mortgage-term', 'value'),
      Input('interest-rate', 'value'),
      Input('offer-term', 'value'),
      Input('initial-interest-rate', 'value'),
      ]
 )
-def plot_monthly_repayments(total, term, interest_rate, offer_term, offer_rate):
+def plot_monthly_repayments(deposit, purchase_price, term, interest_rate, offer_term, offer_rate):
     """
     Callback to plot the payment schedule and populate the total interest repaid.
 
     Args:
-        total ():
+        deposit ():
+        purchase_price ():
         term ():
         interest_rate ():
         offer_term ():
@@ -88,39 +90,49 @@ def plot_monthly_repayments(total, term, interest_rate, offer_term, offer_rate):
     Returns:
 
     """
-    total_borrowed = total * 1000
+    if all(v is not None for v in [deposit, purchase_price, interest_rate, offer_term, offer_rate]):
+        total_borrowed = (purchase_price - deposit) * 1000
 
-    # Compute initial monthly payment
-    offer_m_payment = calc_monthly_payment(total_borrowed, offer_rate, term)
-    initial_payments = np.array([offer_m_payment] * offer_term * 12)
+        # Compute initial monthly payment
+        offer_m_payment = calc_monthly_payment(total_borrowed, offer_rate, term)
+        initial_payments = np.array([offer_m_payment] * offer_term * 12)
 
-    # remaining balance on loan
-    balance = compute_remaining_balance(total_borrowed, offer_rate, offer_term, term)
+        # remaining balance on loan
+        balance = compute_remaining_balance(total_borrowed, offer_rate, offer_term, term)
 
-    # Compute monthly payment according to formula here:
-    remaining_term = (term - offer_term)
-    m_payments = calc_monthly_payment(balance, interest_rate, remaining_term)
-    later_payments = np.array([m_payments] * remaining_term * 12)
+        # Compute monthly payment according to formula here:
+        remaining_term = (term - offer_term)
+        m_payments = calc_monthly_payment(balance, interest_rate, remaining_term)
+        later_payments = np.array([m_payments] * remaining_term * 12)
 
-    # Generate plot data
-    x = np.array(range(1, term * 12 + 1))
-    y = np.append(initial_payments, later_payments)
+        # Generate plot data
+        x = np.array(range(1, term * 12 + 1))
+        y = np.append(initial_payments, later_payments)
 
-    # Create figure dict
-    figure = {
-        'data': [
-            {'x': x,
-             'y': y,
-             'type': 'bar',
-             },
-        ],
-        'layout': {
-            'clickmode': 'event+select'
+        # Create figure dict
+        figure = {
+            'data': [
+                {'x': x,
+                 'y': y,
+                 'type': 'bar',
+                 },
+            ],
+            'layout': {
+                'title': 'Mortgage repayment schedule',
+                'xaxis': {
+                    'title': 'Months'
+                },
+                'yaxis': {
+                    'title': 'Monthly payment (£)'
+                },
+                'clickmode': 'event+select'
+            }
         }
-    }
-    interest_paid = int(np.sum(y) - total_borrowed)
-    total = f"Total interest paid: £{interest_paid:,}"
-    return figure, total
+        interest_paid = int(np.sum(y) - total_borrowed)
+        interest_paid = f"Total interest paid: £{interest_paid:,}"
+        return figure, interest_paid
+    else:
+        raise PreventUpdate
 
 
 def calc_monthly_payment(total_borrowed, r, term):
@@ -133,9 +145,12 @@ def calc_monthly_payment(total_borrowed, r, term):
         r (): annual interest rate (%)
         term (): length of mortgage (years)
     """
+    # Convert from years to months
     n_payments = term * 12
+    # Convert rate to decimal monthly
     offer_r = (r / 12) / 100
     return (total_borrowed * offer_r) / (1 - (1 + offer_r) ** -n_payments)
+
 
 def compute_remaining_balance(total_borrowed, r, offer_term, term):
     """
@@ -151,11 +166,34 @@ def compute_remaining_balance(total_borrowed, r, offer_term, term):
         remaining balance on loan
     """
     # Formula from https://www.mtgprofessor.com/formulas.htm
+    # Convert to decimal monthly interest rate
     offer_r = (r / 12) / 100
+    # Convert from years to months
     offer_months = offer_term * 12
     n_payments = term * 12
+    # Compute remaining balance
     return total_borrowed * ((1 + offer_r) ** n_payments - (1 + offer_r) ** offer_months) / (
-                (1 + offer_r) ** n_payments - 1)
+            (1 + offer_r) ** n_payments - 1)
+
+
+@app.callback(
+    Output('offer-term', 'max'),
+    [Input('mortgage-term', 'value')]
+)
+def limit_offer_term(mortgage_term):
+    """
+    Callback to ensure offer term is shorter than mortgage term
+
+    Args:
+        mortgage_term (): int mortgage term (years)
+
+    Returns:
+        Output('offer-term', 'max') : max value for offer term
+    """
+    if mortgage_term is not None:
+        return mortgage_term - 1
+    else:
+        raise PreventUpdate
 
 
 if __name__ == '__main__':
