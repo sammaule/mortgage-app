93a710d4c18385c68cf191da0ce965ece1f63e10
smaule <smaule@deloitte.co.uk>
2020-06-11 08:53:14 +0100

    Added formulas and docstrings

--- app.py
+++ app.py
@@ -33,19 +33,20 @@ app.layout = html.Div([
              ),
     html.Div([
         "Offer term (years): ",
-        dcc.Input(id='offer-term', value=2, type='number'),
+        # TODO: Write callback so max offer term can't be longer than current mortgage term
+        dcc.Input(id='offer-term', value=2, type='number', min=0),
     ]),
     html.Div([
         "Mortgage term (years): ",
-        dcc.Input(id='mortgage-term', value=20, type='number'),
+        dcc.Input(id='mortgage-term', value=20, type='number', min=0),
     ]),
     html.Div([
         "Initial interest rate (%): ",
-        dcc.Input(id='initial-interest-rate', value=1.0, type='number'),
+        dcc.Input(id='initial-interest-rate', value=1.0, type='number', min=0, max=100),
     ]),
     html.Div([
         "Interest rate (%): ",
-        dcc.Input(id='interest-rate', value=2.0, type='number'),
+        dcc.Input(id='interest-rate', value=2.0, type='number', min=0, max=100),
     ]),
     html.Div(id="total-repaid"
              ),
@@ -74,31 +75,35 @@ def calc_ltv(mortgage, price):
      ]
 )
 def plot_monthly_repayments(total, term, interest_rate, offer_term, offer_rate):
-    # TODO: write helper functions to simplify this callback one for computing monthly payments
-    # One for computing remaining balance
+    """
+    Callback to plot the payment schedule and populate the total interest repaid.
+
+    Args:
+        total ():
+        term ():
+        interest_rate ():
+        offer_term ():
+        offer_rate ():
+
+    Returns:
+
+    """
+    total_borrowed = total * 1000
 
     # Compute initial monthly payment
-    total_borrowed = total * 1000
-    n_payments = term * 12
-    offer_months = offer_term * 12
-    offer_r = (offer_rate / 12) / 100
-    offer_m_payment = (total_borrowed * offer_r) / (1 - (1 + offer_r)**-n_payments)
-    initial_payments = np.array([offer_m_payment] * offer_months)
+    offer_m_payment = calc_monthly_payment(total_borrowed, offer_rate, term)
+    initial_payments = np.array([offer_m_payment] * offer_term * 12)
 
     # remaining balance on loan
-    # Formula from https://www.mtgprofessor.com/formulas.htm
-    balance = total_borrowed * ((1 + offer_r)**n_payments - (1 + offer_r)**offer_months) / ((1 + offer_r)**n_payments - 1)
+    balance = compute_remaining_balance(total_borrowed, offer_rate, offer_term, term)
 
     # Compute monthly payment according to formula here:
-    # https://en.wikipedia.org/wiki/Mortgage_calculator
-
-    n_payments = (term - offer_term) * 12
-    monthly_r = (interest_rate / 12) / 100
-    monthly_payment = (balance * monthly_r) / (1 - (1 + monthly_r)**-n_payments)
-    later_payments = np.array([monthly_payment] * n_payments)
+    remaining_term = (term - offer_term)
+    m_payments = calc_monthly_payment(balance, interest_rate, remaining_term)
+    later_payments = np.array([m_payments] * remaining_term * 12)
 
     # Generate plot data
-    x = np.array(range(1, n_payments + 1))
+    x = np.array(range(1, term * 12 + 1))
     y = np.append(initial_payments, later_payments)
 
     # Create figure dict
@@ -116,6 +121,41 @@ def plot_monthly_repayments(total, term, interest_rate, offer_term, offer_rate):
     interest_paid = int(np.sum(y) - total_borrowed)
     total = f"Total interest paid: £{interest_paid:,}"
     return figure, total
+
+
+def calc_monthly_payment(total_borrowed, r, term):
+    """
+    Function to compute monthly mortgage repayments.
+    https://en.wikipedia.org/wiki/Mortgage_calculator
+
+    Args:
+        total_borrowed (): total amount borrowed
+        r (): annual interest rate (%)
+        term (): length of mortgage (years)
+    """
+    n_payments = term * 12
+    offer_r = (r / 12) / 100
+    return (total_borrowed * offer_r) / (1 - (1 + offer_r) ** -n_payments)
+
+def compute_remaining_balance(total_borrowed, r, offer_term, term):
+    """
+    Computes remaining balance left on loan after end of offer_term periods
+
+    Args:
+        total_borrowed (): total amount initially borrowed
+        r (): interest rate paid (%)
+        offer_term (): length of offer period
+        term (): total length of loan
+
+    Returns:
+        remaining balance on loan
+    """
+    # Formula from https://www.mtgprofessor.com/formulas.htm
+    offer_r = (r / 12) / 100
+    offer_months = offer_term * 12
+    n_payments = term * 12
+    return total_borrowed * ((1 + offer_r) ** n_payments - (1 + offer_r) ** offer_months) / (
+                (1 + offer_r) ** n_payments - 1)
 
 
 if __name__ == '__main__':
