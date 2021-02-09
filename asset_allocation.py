50848694e805e7b8c2cc57cc0268a378a6872694
smaule <smaule@deloitte.co.uk>
2021-02-09 10:35:29 +0000

    Added todo items

--- asset_allocation.py
+++ asset_allocation.py
@@ -1,4 +1,19 @@
 """Contains the layout and callbacks for the Asset allocation page."""
 import dash_html_components as html
 
+# TODO: Add a box containing total wealth from data store
+
+# TODO: Add boxes / sliders allowing allocation of this wealth into:
+#       a) housing
+#       b) cash
+#       c) securities
+
+# TODO: Add boxes for user to fill out the cost / return profiles of each type of investment.
+#       a) housing costs: mortgage, one off (tax etc.), upkeep, bills
+#          housing returns: house price growth, rent
+#       b) cash costs: bank fees?
+#          cash returns: interest rate
+#       c) securities costs: fees
+#          securities returns: expected annual returns
+
 layout = html.Div()
